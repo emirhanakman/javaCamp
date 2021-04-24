cb6006ab2d4540c235c3270d7b6b84f67d9d455a
Emirhan Akman <emirhanakman@aof.anadolu.edu.tr>
2021-04-24 22:04:53 +0300

    update

---
+++ oopIntro/src/oopIntro/Product.java
@@ -0,0 +1,9 @@
+package oopIntro;
+
+public class Product {
+	int id; // ürünlerin benzersiz identity deðeri. Veritabanýnda kayýtlý olan
+	String name; // ürün adi
+	double unitPrice; //birim fiyati
+	String detail; // ürün detayi
+	
+}
